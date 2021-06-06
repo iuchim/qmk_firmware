5fffaadacdf3ec88098d846c55cb5b8d1cc00aba
iuchim <48518312+iuchim@users.noreply.github.com>
2021-06-07 07:27:23 +0900

    update keymap

--- keyboards/iuchim/rkkbd/config.h
+++ keyboards/iuchim/rkkbd/config.h
@@ -54,35 +54,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_HAND_PIN B5
 
 /*
- * RGBLIGHT
+ * RGB Matrix
  */
-#define RGB_DI_PIN D3
-#define RGBLED_NUM 50
-#ifdef RGBLIGHT_ENABLE
-#   define RGBLED_SPLIT { 25, 25 }
-#   define RGBLIGHT_HUE_STEP 8
-#   define RGBLIGHT_SAT_STEP 8
-#   define RGBLIGHT_VAL_STEP 8
-#   define RGBLIGHT_LIMIT_VAL 120 /* The maximum brightness level */
-#   define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*== all animations enable ==*/
-//#    define RGBLIGHT_ANIMATIONS
-/*== or choose animations ==*/
-#   define RGBLIGHT_EFFECT_BREATHING
-#   define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-#   define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#   define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-/*== customize breathing effect ==*/
-/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-#   define RGBLIGHT_BREATHE_TABLE_SIZE 64      // 256(default) or 128 or 64
-/*==== use exp() and sin() ====*/
-//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_DI_PIN D3
+#   define DRIVER_LED_TOTAL 50
+#   define RGB_MATRIX_SPLIT { 25, 25 }
+#   define SPLIT_TRANSPORT_MIRROR
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
+#   define RGB_MATRIX_KEYPRESSES
 #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
